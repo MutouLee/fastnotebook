cd40f55fb2dffe12a3cccd768028c410a3a1672d
Dobeen <23735841@qq.com>
2025-03-26 09:55:26 +0800

    feat: Add category and article route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\ArticleController::class, 'index'])->name('list.index');
+Route::get('/c/{category}', [\App\Http\Controllers\ArticleController::class, 'listByCategory'])->name('list.category');
+Route::get('/{article}', [\App\Http\Controllers\ArticleController::class, 'show'])->where('article', '[0-9]+')->name('article.show');
+Route::get('/getarticles', [\App\Http\Controllers\ArticleController::class, 'getArticlesForList'])->name('list.page');
+Route::get('/s', [\App\Http\Controllers\ArticleController::class, 'search'])->name('article.search');
