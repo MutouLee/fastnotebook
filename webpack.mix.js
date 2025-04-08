c54f3e14633875ce50e7a3c606891d78433747a0
Dobeen <23735841@qq.com>
2025-04-08 15:22:15 +0800

    feat: Change the front-end build environment from Vite to Laravel Mix.

---
+++ webpack.mix.js
@@ -0,0 +1,18 @@
+const mix = require('laravel-mix');
+const glob = require('glob');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+let sassFiles = glob.sync('resources/sass/*.scss');
+for (let file of sassFiles) {
+    mix.sass(file, 'public/css/').version();
+}
